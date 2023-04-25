5d01385c60f791b44775192fdfb683529723fe5f
Alicia Moses <aliciamoses.dev@gmail.com>
2023-04-25 19:15:52 +0100

    Added additional test cases for MeterStats class

--- tests/test_meter_stats.py
+++ tests/test_meter_stats.py
@@ -40,3 +40,34 @@ class TestMeterStats(unittest.TestCase):
         meter_count = meter_stats.count_meters()
         self.assertEqual(meter_count, 2) # Corrected the assertion statement, should have been 2 previously
         
+    def test_sum_valid_readings(self):
+        meter_stats = MeterStats(self.data)
+        _sum = meter_stats.sum_valid_readings()
+        self.assertEqual(_sum, 300.0)
+    
+    def test_sum_invalid_readings(self):
+        meter_stats = MeterStats(self.data)
+        _sum = meter_stats.sum_invalid_readings()
+        self.assertEqual(_sum, 700.0)
+        
+    # Need to pass in the specific key
+        
+    def test_highest_valid_reading(self):
+        meter_stats = MeterStats(self.data)
+        highest = meter_stats.highest_valid_reading()
+        self.assertEqual(highest["VALUE"], 200.0)
+        
+    def test_lowest_valid_reading(self):
+        meter_stats = MeterStats(self.data)
+        lowest = meter_stats.lowest_valid_reading()
+        self.assertEqual(lowest["VALUE"], 100.0)
+        
+    def test_most_recent_reading(self):
+        meter_stats = MeterStats(self.data)
+        most_recent = meter_stats.most_recent_reading()
+        self.assertEqual(most_recent["READING_ID"], "READING_ID_4")
+        
+    def test_most_recent_reading(self):
+        meter_stats = MeterStats(self.data)
+        least_recent = meter_stats.least_recent_reading()
+        self.assertEqual(least_recent["READING_ID"], "READING_ID_1")
