24f16126d79821da442f3d56abf947b900cd3c11
Alicia Moses <aliciamoses.dev@gmail.com>
2023-04-25 17:13:31 +0100

    Passing unittest for FileReader class

--- file_reader.py
+++ file_reader.py
@@ -1,4 +1,12 @@
 
 class FileReader():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, file_path):
+        self.file_path = file_path
+        
+    def read_file(self):
+       with open(self.file_path, 'r') as file:
+           content = file.read()
+       return content 
+   
+   
+   
