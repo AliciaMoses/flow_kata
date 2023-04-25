505514664b818b3b45ed1874805e342b2e610073
Alicia Moses <aliciamoses.dev@gmail.com>
2023-04-25 18:03:24 +0100

    Added test case for DataParser class: with this commit I have created a test case with some example data in the file schema format I have created data that will reflect the desired output with the correct data types and structure

---
+++ data_parser.py
@@ -0,0 +1,4 @@
+
+class DataParser():
+    def __init__(self) -> None:
+        pass
---
+++ tests/test_data_parser.py
@@ -0,0 +1,53 @@
+import unittest
+from data_parser import DataParser
+from datetime import datetime
+
+
+class TestDataParser(unittest.TestCase):
+    def test_parse_data(self):
+        content = (
+            "HEADER |\n"
+            "METER | METER_ID_1\n"
+            "READING | READING_ID_1 | 100 | 20230401 | V |\n"
+            "READING | READING_ID_2 | 200 | 20230402 | F | \n"
+            "METER | METER_ID_2\n"
+            "READING | READING_ID_3 | 200 | 20230405 | V |\n"
+            "READING | READING_ID_4 | 500 | 20230406 | F |\n"
+            "FOOTER "
+        )
+
+        expected_data = [
+            {
+                "METER_ID": "METER_ID_1",
+                "READING_ID": "READING_ID_1",
+                "VALUE": 100.0,
+                "DATE": datetime(2023, 4, 1),
+                "STATUS": "V",
+            },
+            {
+                "METER_ID": "METER_ID_1",
+                "READING_ID": "READING_ID_2",
+                "VALUE": 200.0,
+                "DATE": datetime(2023, 4, 2),
+                "STATUS": "F",
+            },
+            {
+                "METER_ID": "METER_ID_2",
+                "READING_ID": "READING_ID_3",
+                "VALUE": 200.0,
+                "DATE": datetime(2023, 4, 5),
+                "STATUS": "V",
+            },
+            {
+                "METER_ID": "METER_ID_2",
+                "READING_ID": "READING_ID_4",
+                "VALUE": 500.0,
+                "DATE": datetime(2023, 4, 6),
+                "STATUS": "F",
+            },
+        ]
+
+        data_parser = DataParser(content)
+        parsed_data = data_parser.parse_data()
+        
+        self.assertEqual(parsed_data, expected_data)
