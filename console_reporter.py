48d358880c9fa01f88df93dd09a8adfd27ad07f4
Alicia Moses <aliciamoses.dev@gmail.com>
2023-04-25 20:29:21 +0100

    Passing tests for printing sum of meter readings by status

--- console_reporter.py
+++ console_reporter.py
@@ -8,3 +8,11 @@ class ConsoleReporter():
     def print_meter_count(self):
         meter_count = self.meter_stats.count_meters()
         print("Count of meters:", meter_count)
+        
+    def print_sum_valid_readings(self):
+        meter_count = self.meter_stats.sum_valid_readings()
+        print("Sum of valid meter readings:", meter_count)
+        
+    def print_sum_invalid_readings(self):
+        meter_count = self.meter_stats.sum_invalid_readings()
+        print("Sum of invalid meter readings:", meter_count)
