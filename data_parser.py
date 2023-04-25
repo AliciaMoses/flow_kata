f1866224e732dc6a91f8c65d8937295517997e34
Alicia Moses <aliciamoses.dev@gmail.com>
2023-04-25 18:31:12 +0100

    Passing test for DataParser class: with this commit I have used the typing module to add functionality to the DataParser class whilst specifying types, it converts the file data into the specified data types, stores it in a dictionary and appeands it to a list

--- data_parser.py
+++ data_parser.py
@@ -1,4 +1,58 @@
+# I will be using the typing module here for consistency and readability
+
+from typing import List, Dict, Union
+from datetime import datetime
 
 class DataParser():
-    def __init__(self) -> None:
-        pass
+    def __init__(self, content: str):
+        self.content = content
+        
+    # using typing to define the correct data types and structure 
+    # the structure is a list of dictionaries with strings, float val and datetime object   
+    def parse_data(self) -> List[Dict[str, Union[str, float, datetime]]]:
+        
+        # now need to initialise an empty list for the data
+        
+        parsed_data = []
+        
+        # split the main string (within the file) up by line
+        lines = self.content.split('\n')
+        
+        # iterate over the lines
+        
+        for line in lines:
+            #skip over empty lines, header and footer
+            if line.startswith("HEADER") or line.startswith("FOOTER") or not line.strip():
+                continue
+            
+            # create fields from the line
+            fields = line.split("|")
+            
+            # extract data from fields
+            record_type = fields[0].strip()
+            
+            if record_type == "METER":
+                # Define as meter_id
+                meter_id = fields[1].strip()
+                # taking the next value and saving that as a variable called meter_id
+            elif record_type == "READING":
+                reading_id = fields[1].strip()
+                # define this as type float, since the others were str by default
+                value = float(fields[2].strip())
+                date_str = fields[3].strip()
+                status = fields[4].strip()
+                
+                # convert date_str into the datetime object
+                date = datetime.strptime(date_str, "%Y%m%d")
+                
+                # add the parsed data to the list of dictionaries
+                
+                parsed_data.append({
+                    "METER_ID": meter_id,
+                    "READING_ID": reading_id,
+                    "VALUE": value,
+                    "DATE": date,
+                    "STATUS": status  
+                })
+        
+        return parsed_data 
--- tests/test_data_parser.py
+++ tests/test_data_parser.py
@@ -13,8 +13,13 @@ class TestDataParser(unittest.TestCase):
             "METER | METER_ID_2\n"
             "READING | READING_ID_3 | 200 | 20230405 | V |\n"
             "READING | READING_ID_4 | 500 | 20230406 | F |\n"
+            "METER | METER_ID_1\n"
+            "READING | READING_ID_5 | 400 | 20230405 | V |\n"
+            "READING | READING_ID_6 | 600 | 20230406 | F |\n"
             "FOOTER "
         )
+        
+        # going to add another example to the data
 
         expected_data = [
             {
@@ -45,9 +50,24 @@ class TestDataParser(unittest.TestCase):
                 "DATE": datetime(2023, 4, 6),
                 "STATUS": "F",
             },
+              {
+                "METER_ID": "METER_ID_1",
+                "READING_ID": "READING_ID_5",
+                "VALUE": 400.0,
+                "DATE": datetime(2023, 4, 5),
+                "STATUS": "V",
+            },
+            {
+                "METER_ID": "METER_ID_1",
+                "READING_ID": "READING_ID_6",
+                "VALUE": 600.0,
+                "DATE": datetime(2023, 4, 6),
+                "STATUS": "F",
+            },
         ]
 
         data_parser = DataParser(content)
         parsed_data = data_parser.parse_data()
         
         self.assertEqual(parsed_data, expected_data)
+
